9a2196e818d58cf7eb9a061ca6c15fedc9af9574
Eleonora Mattera <m.eleonora98@live.it>
2023-09-18 20:19:19 +0200

    update Meteo

--- src/components/Meteo/Meteo.jsx
+++ src/components/Meteo/Meteo.jsx
@@ -8,21 +8,24 @@ function Meteo({ meteo }) {
         {meteo.map((meteo) => {
           return (
             <Col xs={12} sm={6} md={4} lg={3} className="mb-5">
-              <Card class="shadow p-3 mb-5 bg-body rounded">
+              <Card class="shadow p-3 mb-5 bg-body rounded ">
                 <Card.Body>
-                  <h2 className="text-center fw-bold fs-1"> {meteo.main.temp}°C</h2>
+                  <Card.Text>
+                    <h5>Giorno: {new Date(meteo.dt_txt).toLocaleDateString("it-IT")}</h5>
+                    <h5>Ora: {new Date(meteo.dt_txt).toLocaleTimeString("it-IT")}</h5>
+                  </Card.Text>
 
                   <Card.Img
                     variant="top"
-                    className="w-50 mx-5"
+                    className=" mx-5 position-absolute start-0 top-0 mt-2 ms-2"
                     src={`http://openweathermap.org/img/w/${meteo.weather[0].icon}.png?mode=png`}
                     alt="weather"
-                    width="40%"
+                    width="10px"
+                    style={{ zIndex: -1, filter: "blur(4px) opacity(0.9)" }}
                   />
 
                   <Card.Text>
-                    <p>Giorno {new Date(meteo.dt_txt).toLocaleDateString("it-IT")}</p>
-                    <p>Ora {new Date(meteo.dt_txt).toLocaleTimeString("it-IT")}</p>
+                    <p className="text-center fw-bold "> {meteo.main.temp}°F</p>
                     <p>
                       {meteo.weather[0].main} <br></br> {meteo.weather[0].description}
                     </p>
