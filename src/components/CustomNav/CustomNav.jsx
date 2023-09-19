c76992ae8d3c7f73c7f74234f24469ee3b78f4db
Eleonora Mattera <m.eleonora98@live.it>
2023-09-19 23:57:59 +0200

    Update CustomNav

--- src/components/CustomNav/CustomNav.jsx
+++ src/components/CustomNav/CustomNav.jsx
@@ -1,8 +1,3 @@
-// import Container from "react-bootstrap/Container";
-// import Navbar from "react-bootstrap/Navbar";
-// import Nav from "react-bootstrap/Nav";
-// import NavDropdown from "react-bootstrap/NavDropdown";
-
 import { Container, Navbar, Nav } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
 import meteo from "../../assets/img/meteo.png";
@@ -13,21 +8,10 @@ const CustomNav = () => {
   console.log("LOCATION", location);
 
   return (
-    <Navbar
-      id="mynav"
-      style={{
-        width: "20%",
-        height: "100%",
-        borderRadius: "30px",
-        backgroundColor: "red",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        margin: "10px",
-      }}>
+    <Navbar>
       <Container className="d-flex, flex-column">
         <Navbar.Brand href="#home">
-          <img src={meteo} alt="logo meteo" style={{ width: "200px", marginTop: "20px" }} />
+          <img src={meteo} alt="logo meteo" className="logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
@@ -39,7 +23,7 @@ const CustomNav = () => {
           </div>
           <div>
             <Link className={`nav-link ${location.pathname === "/previsioni" ? "active" : ""}`} to="/previsioni">
-              Previsioni
+              Forecast
             </Link>
           </div>
         </Nav>
