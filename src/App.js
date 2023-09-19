b922f9d475c321d94458a7816d74d6f76feac610
Eleonora Mattera <m.eleonora98@live.it>
2023-09-19 23:57:48 +0200

    update App.css App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./components/Home/Home";
 
-import Previsioni from "./components/Previsioni";
+import Previsioni from "./components/Previsioni/Previsioni.jsx";
 import CustomNav from "./components/CustomNav/CustomNav.jsx";
 
 function App() {
   return (
     <>
-      <Container fluid className="d-flex p-0" style={{ height: "100%" }}>
+      <Container fluid className="d-flex p-0 h-100">
         <BrowserRouter>
           <CustomNav />
           <Routes>
